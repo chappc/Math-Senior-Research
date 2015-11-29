ce2d044572ec28df0caa1c98f9f1a5bd5f0d2cb2
James Hoctor <JEHoctor@gmail.com>
2015-11-29 15:22:57 -0500

    It makes more sense for the OpenGL version dumping code to live in its own file. If this file, openGLinfo.py, is called as a script, it will dump the info and exit. If it is imported as a module, it makes available a single function called print_openGL_info which prints the information so long as a valid openGL context is open.

---
+++ openGLinfo.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+import pygame
+import OpenGL
+OpenGL.USE_ACCELERATOR = True
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+import sys
+from ADParser import parse_file
+
+def print_openGL_info():
+    # Query OpenGL for version information:
+    print 'glGetIntegerv(GL_MAJOR_VERSION) =',glGetIntegerv(GL_MAJOR_VERSION)
+    print 'glGetIntegerv(GL_MINOR_VERSION) =',glGetIntegerv(GL_MINOR_VERSION)
+    print 'glGetString(GL_VERSION) = \''+glGetString(GL_VERSION)+'\''
+    print 'glGetString(GL_VENDOR) = \''+glGetString(GL_VENDOR)+'\''
+    print 'glGetString(GL_RENDERER) = \''+glGetString(GL_RENDERER)+'\''
+    n_extensions = glGetIntegerv(GL_NUM_EXTENSIONS)
+    print 'glGetIntegerv(GL_NUM_EXTENSIONS) =',n_extensions
+    for i in xrange(n_extensions):
+        print 'glGetStringi(GL_EXTENSIONS, '+str(i)+') = \''+glGetStringi(GL_EXTENSIONS, i)+'\''
+    print 'glGetString(GL_SHADING_LANGUAGE_VERSION) = \''+glGetString(GL_SHADING_LANGUAGE_VERSION)+'\''
+    # The following doesn't seem to be supported by pyOpenGL:
+    # n_supported_GLSL_vers = glGetIntegerv(GL_NUM_SHADING_LANGUAGE_VERSIONS)
+    # print 'glGetIntegerv(GL_NUM_SHADING_LANGUAGE_VERSIONS) =',n_supported_GLSL_vers
+        # for i in xrange(n_supported_GLSL_vers):
+    # print 'glGetStringi(GL_SHADING_LANGUAGE_VERSION, '+str(i)+') = \''+glGetStringi(GL_SHADING_LANGUAGE_VERSION, i)+'\''
+    
+if __name__=="__main__":
+    pygame.init()
+    display = (100,100)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    print_openGL_info()
